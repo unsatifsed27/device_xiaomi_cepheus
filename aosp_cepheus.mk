c2bbec91cc826182ece9169aa52e8dc5123dfe48
dark-free <emchaouki@gmail.com>
2021-10-22 09:29:58 +0000

    adatp to AOSP ROMS

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -22,23 +22,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/kangos/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+IS_PHONE := true
+CUSTOM_BUILD_TYPE=OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_PRODUCT_PROPERTIES += \
-  ro.kangos.maintainer=freesoul00 \
-  ro.kangos.cpu=SDM855
-
-
 # Device identifier
-PRODUCT_NAME := kangos_cepheus
+PRODUCT_NAME := aosp_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
 PRODUCT_MANUFACTURER := Xiaomi
-KANGOS_BUILDTYPE := OFFICIAL
 
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
